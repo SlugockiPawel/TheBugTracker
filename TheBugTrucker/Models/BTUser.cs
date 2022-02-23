bf3ab47d9ae895c5f87be27efcdc89f0e5fc152f
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-23 16:45:25 +0100

    implement BTUser model
    BTUser = BugTruckerUser

---
+++ TheBugTrucker/Models/BTUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TheBugTrucker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = default!;
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = default!;
+
+        [NotMapped][Display(Name = "Full Name")]  public string FullName => $"{FirstName} {LastName}";
+    }
+}
