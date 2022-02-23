87c5055282bd427b6eec7c5425b4d7d4eb6dadfc
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-23 17:11:36 +0100

    make IdentityDbContext use BTUSer type (it would use default IdentityUser instead)

--- TheBugTrucker/Data/ApplicationDbContext.cs
+++ TheBugTrucker/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TheBugTrucker.Models;
 
 namespace TheBugTrucker.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    /// <summary>
+    /// This class is responsible for connecting to database.
+    /// As the custom identity user was introduced, IdentityDbContext should accept BTUser type
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
