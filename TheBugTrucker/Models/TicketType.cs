ced2b6c1a7f27d682018feea2a346fabe9d50728
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-25 12:23:25 +0100

    implement TicketType model

---
+++ TheBugTrucker/Models/TicketType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTrucker.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Type Name")]
+        public string Name { get; set; } = default!;
+    }
+}
