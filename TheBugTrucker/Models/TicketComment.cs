ff3dbf99a90973e465f2e4c93bbfd6706116648e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-02-28 11:20:51 +0100

    implement TicketComment model

---
+++ TheBugTrucker/Models/TicketComment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTrucker.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")] public string Comment { get; set; } = default!;
+
+        [DisplayName("Date")] public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")] public int TicketId { get; set; }
+
+        [DisplayName("Team Member")] public string UserId { get; set; } = default!;
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; } = default!;
+    }
+}
