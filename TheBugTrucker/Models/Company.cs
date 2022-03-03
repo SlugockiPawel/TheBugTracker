559c16c449048476408804b45530d806d6d4db74
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-03 13:07:15 +0100

    implement Company model

---
+++ TheBugTrucker/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTrucker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Company Name")] public string Name { get; set; } = default!;
+        [DisplayName("Company Description")] public string Description { get; set; } = default!;
+
+        // NP
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+        public virtual ICollection<Project> Project { get; set; } = new HashSet<Project>();
+    }
+}
