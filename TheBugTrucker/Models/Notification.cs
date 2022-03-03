5bed1b866984a82c98bfe2c04b4e7e7b6eb9c6b6
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-03 13:07:29 +0100

    implement Notification model

---
+++ TheBugTrucker/Models/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTrucker.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")] public int TicketId { get; set; }
+
+        [Required] public string Title { get; set; } = default!;
+
+        [Required] public string Message { get; set; } = default!;
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [Required] [DisplayName("Recipient")] public string RecipientId { get; set; } = default!;
+        [Required] [DisplayName("Sender")] public string SenderId { get; set; } = default!;
+
+        [DisplayName("Has been viewed")] public bool Viewed { get; set; }
+
+        // NP
+        public virtual Ticket Ticket { get; set; } = default!;
+        public virtual BTUser Recipient { get; set; } = default!;
+        public virtual BTUser Sender { get; set; } = default!;
+    }
+}
