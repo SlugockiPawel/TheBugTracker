f67d3410d6264bf6e643b21dd5af1b6cae288828
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-03 13:07:39 +0100

    add invite model

---
+++ TheBugTrucker/Models/Invite.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace TheBugTrucker.Models
+{
+    public class Invite
+    {
+        // PK
+        public int Id { get; set; }
+
+        // FK
+        [DisplayName("Company")] public int CompanyId { get; set; }
+        [DisplayName("Project")] public int ProjectId { get; set; }
+        [DisplayName("Invitor")] public string InvitorId { get; set; } = default!;
+        [DisplayName("Invitee")] public string InviteeId { get; set; } = default!;
+
+        [DisplayName("Invitee Email")] public string InviteeEmail { get; set; } = default!;
+        [DisplayName("Invitee First Name")] public string InviteeFirstName { get; set; } = default!;
+        [DisplayName("Invitee Last Name")] public string InviteeLastName { get; set; } = default!;
+        [DisplayName("Date Sent")] public DateTimeOffset InviteDate { get; set; }
+        [DisplayName("Join Date")] public DateTimeOffset JoinDate { get; set; }
+        [DisplayName("Code")] public Guid CompanyToken { get; set; }
+        public bool IsValid { get; set; }
+
+        // NP
+        public virtual Company Company { get; set; } = default!;
+        public virtual Project Project { get; set; } = default!;
+        public virtual BTUser Invitor { get; set; } = default!;
+        public virtual BTUser Invitee { get; set; } = default!;
+    }
+}
