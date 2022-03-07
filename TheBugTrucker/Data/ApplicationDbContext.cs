ab64102bd5fdfbb4d8534b6401fa34291392f548
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-07 17:20:07 +0100

    as DbSets are nullable, add default!;

--- TheBugTrucker/Data/ApplicationDbContext.cs
+++ TheBugTrucker/Data/ApplicationDbContext.cs
@@ -15,17 +15,17 @@ namespace TheBugTrucker.Data
         {
         }
 
-        public DbSet<Company> Companies { get; set; }
-        public DbSet<Invite> Invites { get; set; }
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<Ticket> Tickets { get; set; }
-        public DbSet<Notification> Notifications { get; set; }
-        public DbSet<ProjectPriority> ProjectPriorities { get; set; }
-        public DbSet<TicketAttachment> TicketAttachments { get; set; }
-        public DbSet<TicketComment> TicketComments { get; set; }
-        public DbSet<TicketHistory> TicketHistories { get; set; }
-        public DbSet<TicketPriority> TicketPriorities { get; set; }
-        public DbSet<TicketStatus> TicketStatuses { get; set; }
-        public DbSet<TicketType> TicketTypes { get; set; }
+        public DbSet<Company> Companies { get; set; } = default!;
+        public DbSet<Invite> Invites { get; set; } = default!;
+        public DbSet<Project> Projects { get; set; } = default!;
+        public DbSet<Ticket> Tickets { get; set; } = default!;
+        public DbSet<Notification> Notifications { get; set; } = default!;
+        public DbSet<ProjectPriority> ProjectPriorities { get; set; } = default!;
+        public DbSet<TicketAttachment> TicketAttachments { get; set; } = default!;
+        public DbSet<TicketComment> TicketComments { get; set; } = default!;
+        public DbSet<TicketHistory> TicketHistories { get; set; } = default!;
+        public DbSet<TicketPriority> TicketPriorities { get; set; } = default!;
+        public DbSet<TicketStatus> TicketStatuses { get; set; } = default!;
+        public DbSet<TicketType> TicketTypes { get; set; } = default!;
     }
 }
