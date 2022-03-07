c152119f30403cf4b8d24a2d5c2a1b60c23f822b
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-07 11:47:27 +0100

    implement ICompanyInfoService

---
+++ TheBugTrucker/Services/Interfaces/ICompanyInfoService.cs
@@ -0,0 +1,12 @@
+﻿using TheBugTrucker.Models;
+
+namespace TheBugTrucker.Services.Interfaces
+{
+    public interface ICompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId);
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+        public Task<List<Ticket>> GetAllTicketsAsync(int projectId);
+    }
+}
