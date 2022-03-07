4cea4c51897b61787c20b3a925048693b1c2167e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-07 17:19:02 +0100

    bug fix: rename parameter name

--- TheBugTrucker/Services/Interfaces/ICompanyInfoService.cs
+++ TheBugTrucker/Services/Interfaces/ICompanyInfoService.cs
@@ -7,6 +7,6 @@ namespace TheBugTrucker.Services.Interfaces
         public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
         public Task<List<BTUser>> GetAllMembersAsync(int companyId);
         public Task<List<Project>> GetAllProjectsAsync(int companyId);
-        public Task<List<Ticket>> GetAllTicketsAsync(int projectId);
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId);
     }
 }
