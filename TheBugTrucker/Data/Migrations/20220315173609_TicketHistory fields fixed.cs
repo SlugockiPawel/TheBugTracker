20eb2a09073d8b8d557949e90b67525e34279675
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-15 18:42:02 +0100

    add migration that reflect changes in the previous commit and update database

---
+++ TheBugTrucker/Data/Migrations/20220315173609_TicketHistory fields fixed.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBugTrucker.Data.Migrations
+{
+    public partial class TicketHistoryfieldsfixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Property",
+                table: "TicketHistories",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OldValue",
+                table: "TicketHistories",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NewValue",
+                table: "TicketHistories",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Property",
+                table: "TicketHistories",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OldValue",
+                table: "TicketHistories",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewValue",
+                table: "TicketHistories",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
