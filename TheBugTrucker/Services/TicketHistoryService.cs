4747771dbff4c5a04f90df59081f0ff57d4bc580
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-15 12:34:52 +0100

    add TicketHistoryService

---
+++ TheBugTrucker/Services/TicketHistoryService.cs
@@ -0,0 +1,23 @@
+﻿using TheBugTrucker.Models;
+using TheBugTrucker.Services.Interfaces;
+
+namespace TheBugTrucker.Services
+{
+    public class TicketHistoryService : ITicketHistoryService
+    {
+        public Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
