c658ff9dfe6f01f921bf564dbe41e42b8e3b2423
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-18 13:43:20 +0100

    implement INotificationService

---
+++ TheBugTrucker/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using TheBugTrucker.Models;
+
+namespace TheBugTrucker.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        Task AddNotificationAsync(Notification notification);
+        Task<List<Notification>> GetSentNotificationsAsync(string userId);
+        Task<List<Notification>> GetReceivedNotificationsAsync(string userId);
+        Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role);
+        Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members);
+        Task SendEmailNotificationAsync(Notification notification, string emailSubject);
+    }
+}
