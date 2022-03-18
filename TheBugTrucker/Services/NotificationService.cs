d6d3ce984f13937a306a418608d544a26b2e22c2
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-18 15:46:09 +0100

    implement AddNotificationAsync()

--- TheBugTrucker/Services/NotificationService.cs
+++ TheBugTrucker/Services/NotificationService.cs
@@ -1,13 +1,34 @@
-﻿using TheBugTrucker.Models;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using TheBugTrucker.Data;
+using TheBugTrucker.Models;
 using TheBugTrucker.Services.Interfaces;
 
 namespace TheBugTrucker.Services
 {
     public class NotificationService : INotificationService
     {
-        public Task AddNotificationAsync(Notification notification)
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IRolesService _rolesService;
+        public NotificationService(ApplicationDbContext context, IEmailSender emailSender, IRolesService rolesService)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _emailSender = emailSender;
+            _rolesService = rolesService;
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            try
+            {
+                await _context.Notifications.AddAsync(notification);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
 
         public Task<List<Notification>> GetSentNotificationsAsync(string userId)
