d30d2cabf72b4cd506fcd0f0d028c1a4d00a2056
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-18 15:37:49 +0100

    add NotificationService()

---
+++ TheBugTrucker/Services/NotificationService.cs
@@ -0,0 +1,38 @@
+﻿using TheBugTrucker.Models;
+using TheBugTrucker.Services.Interfaces;
+
+namespace TheBugTrucker.Services
+{
+    public class NotificationService : INotificationService
+    {
+        public Task AddNotificationAsync(Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
