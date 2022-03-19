c0531c52689cbd42d8f63100aa346603b37066a9
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-19 17:47:47 +0100

    add InviteService

---
+++ TheBugTrucker/Services/InviteService.cs
@@ -0,0 +1,46 @@
+﻿using TheBugTrucker.Data;
+using TheBugTrucker.Models;
+using TheBugTrucker.Services.Interfaces;
+
+namespace TheBugTrucker.Services
+{
+    public class InviteService : IInviteService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InviteService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewInviteAsync(Invite invite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(int inviteId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateInviteCodeAsync(Guid? token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
