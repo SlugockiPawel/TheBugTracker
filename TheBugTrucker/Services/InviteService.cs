dfff185c8a7637bae7c843a69a43a65c60fe0480
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-20 16:21:22 +0100

    implement GetInviteAsync() overload

--- TheBugTrucker/Services/InviteService.cs
+++ TheBugTrucker/Services/InviteService.cs
@@ -83,9 +83,22 @@ namespace TheBugTrucker.Services
             }
         }
 
-        public Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+        public async Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _context.Invites
+                    .Where(i => i.CompanyId == companyId)
+                    .Include(i => i.Company)
+                    .Include(i => i.Project)
+                    .Include(i => i.Invitor)
+                    .FirstOrDefaultAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
 
         public Task<bool> ValidateInviteCodeAsync(Guid? token)
