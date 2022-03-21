5a8eee93ca0b7bf64ca6156d09526718543ae290
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-21 15:55:48 +0100

    add FileService

---
+++ TheBugTrucker/Services/FileService.cs
@@ -0,0 +1,27 @@
+﻿using TheBugTrucker.Services.Interfaces;
+
+namespace TheBugTrucker.Services
+{
+    public class FileService : IFileService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileIcon(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatFileSize(long bytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
