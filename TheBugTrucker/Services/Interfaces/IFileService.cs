64701bb9d96237d2403a31f8875725a07fa5ef08
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-21 15:34:00 +0100

    implement IFileService

---
+++ TheBugTrucker/Services/Interfaces/IFileService.cs
@@ -0,0 +1,10 @@
+﻿namespace TheBugTrucker.Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+        string GetFileIcon(string file);
+        string FormatFileSize(long bytes);
+    }
+}
