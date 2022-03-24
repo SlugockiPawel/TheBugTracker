03aaa0216a2e5cb03038362d6d8f26e5a24c1d39
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-24 18:28:22 +0100

    override GenerateClaimsAsync()

---
+++ TheBugTrucker/Services/Factories/UserClaimsPrincipalFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using TheBugTrucker.Models;
+
+namespace TheBugTrucker.Services.Factories
+{
+    public class UserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
+    {
+        public UserClaimsPrincipalFactory(UserManager<BTUser> userManager, RoleManager<IdentityRole> roleManager,
+            IOptions<IdentityOptions> options)
+            : base(userManager, roleManager, options)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user)
+        {
+            ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+
+            return identity;
+        }
+    }
+}
