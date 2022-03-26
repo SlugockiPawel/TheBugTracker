ba09ef57c4f278b975e94883180276e23cfa0403
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-26 14:19:22 +0100

    add UserRolesController

---
+++ TheBugTrucker/Controllers/UserRolesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Evaluation;
+using TheBugTrucker.Services.Interfaces;
+
+namespace TheBugTrucker.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly IRolesService _rolesService;
+        private readonly ICompanyInfoService _companyInfoService;
+
+        public UserRolesController(IRolesService rolesService, ICompanyInfoService companyInfoService)
+        {
+            _rolesService = rolesService;
+            _companyInfoService = companyInfoService;
+        }
+
+        public async Task<IActionResult> ManageUserRoles()
+        {
+            return View();
+        }
+    }
+}
