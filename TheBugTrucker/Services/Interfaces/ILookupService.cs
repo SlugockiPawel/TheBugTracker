6cae95703eb41ba4cc7d5a70b86e607142c2aa26
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-29 13:47:50 +0200

    implement ILookupService

---
+++ TheBugTrucker/Services/Interfaces/ILookupService.cs
@@ -0,0 +1,12 @@
+﻿using TheBugTrucker.Models;
+
+namespace TheBugTrucker.Services.Interfaces
+{
+    public interface ILookupService
+    {
+        Task<List<TicketPriority>> GetTicketPrioritiesAsync();
+        Task<List<TicketStatus>> GetTicketStatusesAsync();
+        Task<List<TicketType>> GetTicketTypesAsync();
+        Task<List<ProjectPriority>> GetProjectPrioritiesAsync();
+    }
+}
