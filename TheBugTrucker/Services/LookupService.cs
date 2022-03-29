cc078df5780e174be66b28561d7f79bdfcf7eb91
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-29 13:48:02 +0200

    implement LookupService

---
+++ TheBugTrucker/Services/LookupService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBugTrucker.Data;
+using TheBugTrucker.Models;
+using TheBugTrucker.Services.Interfaces;
+
+namespace TheBugTrucker.Services
+{
+    public class LookupService : ILookupService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LookupService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<TicketPriority>> GetTicketPrioritiesAsync()
+        {
+            try
+            {
+                return await _context.TicketPriorities.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public async Task<List<TicketStatus>> GetTicketStatusesAsync()
+        {
+            try
+            {
+                return await _context.TicketStatuses.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public async Task<List<TicketType>> GetTicketTypesAsync()
+        {
+            try
+            {
+                return await _context.TicketTypes.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        public async Task<List<ProjectPriority>> GetProjectPrioritiesAsync()
+        {
+            try
+            {
+                return await _context.ProjectPriorities.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
