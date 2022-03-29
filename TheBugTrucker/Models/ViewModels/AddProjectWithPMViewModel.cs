2fefdfae15c07d60abc55800d272856bfa95aa6d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-03-29 15:16:20 +0200

    implement AddProjectWithPMViewModel

---
+++ TheBugTrucker/Models/ViewModels/AddProjectWithPMViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TheBugTrucker.Models.ViewModels
+{
+    public class AddProjectWithPMViewModel
+    {
+        public Project Project { get; set; } = default!;
+        public SelectList PMList { get; set; } = default!;
+        public string PmId { get; set; } = default!;
+        public SelectList PriorityList { get; set; } = default!;
+        public int ProjectPriority { get; set; } = default!;
+    }
+}
