ae5c2e574cb218a03b486bf3fee488f5fb8c185f
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-04-13 14:49:51 +0200

    Revert "implement implement Archive POST method"
    This reverts commit 6ee5fed44cbfb65308e04df178effb31ca8739ef.

--- TheBugTracker/Controllers/TicketsController.cs
+++ TheBugTracker/Controllers/TicketsController.cs
@@ -230,45 +230,14 @@ namespace TheBugTracker.Controllers
             return View(ticket);
         }
 
-        // POST: Tickets/Archive/5
-        [HttpPost, ActionName("Archive")]
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ArchiveConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
-            ticket.Archived = true;
-            await _ticketService.UpdateTicketAsync(ticket);
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        // GET: Tickets/Restore/5
-        public async Task<IActionResult> Restore(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Ticket ticket = await _ticketService.GetTicketByIdAsync(id.Value);
-
-            if (ticket == null)
-            {
-                return NotFound();
-            }
-
-            return View(ticket);
-        }
-
-        // POST: Tickets/Restore/5
-        [HttpPost, ActionName("Restore")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> RestoreConfirmed(int id)
-        {
-            Ticket ticket = await _ticketService.GetTicketByIdAsync(id);
-            ticket.Archived = false;
-            await _ticketService.UpdateTicketAsync(ticket);
-
+            var ticket = await _context.Tickets.FindAsync(id);
+            _context.Tickets.Remove(ticket);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
