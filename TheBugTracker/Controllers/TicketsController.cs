32706109cf0f9a781764b08785cdea386160e199
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-04-13 15:13:35 +0200

    implement MyTickets GET method

--- TheBugTracker/Controllers/TicketsController.cs
+++ TheBugTracker/Controllers/TicketsController.cs
@@ -43,6 +43,15 @@ namespace TheBugTracker.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+        // GET: MyTickets
+        public async Task<IActionResult> MyTickets()
+        {
+            BTUser user = await _userManager.GetUserAsync(User);
+            List<Ticket> tickets = await _ticketService.GetTicketsByUserIdAsync(user.Id, user.CompanyId);
+
+            return View(tickets);
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
