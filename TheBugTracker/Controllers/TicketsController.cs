ff17026e1f9eb61bb974493249a312b831dfd32e
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-04-15 13:20:48 +0200

    implement ArchivedTickets GET method

--- TheBugTracker/Controllers/TicketsController.cs
+++ TheBugTracker/Controllers/TicketsController.cs
@@ -65,6 +65,15 @@ namespace TheBugTracker.Controllers
             return View(tickets);
         }
 
+        // GET: ArchivedTickets
+        public async Task<IActionResult> ArchivedTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+            List<Ticket> tickets =  await _ticketService.GetArchivedTicketsAsync(companyId);
+
+            return View(tickets);
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
