6c8706603ac0d460a9fa7ece3d29f857f10720c7
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-06-18 17:46:50 +0200

    update TODO list

--- TheBugTracker/Program.cs
+++ TheBugTracker/Program.cs
@@ -8,11 +8,12 @@ using TheBugTracker.Services.Factories;
 using TheBugTracker.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// TODO store all passwords and emails in the secrets.json! Pass iConfiguration to SeedDeaultUsersAsync method
 // TODO implement notifications mechanism during new Ticket creation
 // TODO add filters to tables so user can filter tickets by priority etc.
 // TODO Add Index page where user can login as Demo Admin / Demo PM/ Demo Developer/ Demo User
 // TODO Should Company list be visible for all users?
+// TODO Change Priority tags colors based on Priority Name
 
 // Add services to the container.
 var connectionString = DataUtility.GetConnectionString(builder);
