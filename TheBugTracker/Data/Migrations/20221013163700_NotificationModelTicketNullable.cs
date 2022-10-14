14d640d4034f1a21f151a6faa2c7828e3edcbe54
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-10-14 12:06:25 +0200

    add migration

---
+++ TheBugTracker/Data/Migrations/20221013163700_NotificationModelTicketNullable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBugTracker.Data.Migrations
+{
+    public partial class NotificationModelTicketNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Tickets_TicketId",
+                table: "Notifications");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketId",
+                table: "Notifications",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Tickets_TicketId",
+                table: "Notifications",
+                column: "TicketId",
+                principalTable: "Tickets",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Tickets_TicketId",
+                table: "Notifications");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TicketId",
+                table: "Notifications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Tickets_TicketId",
+                table: "Notifications",
+                column: "TicketId",
+                principalTable: "Tickets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
