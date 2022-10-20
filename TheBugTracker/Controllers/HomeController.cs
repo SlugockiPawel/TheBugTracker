e5de03a6f10c7ff275760003aaaf4896f440bd24
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2022-10-20 16:00:53 +0200

    reformat code

--- TheBugTracker/Controllers/HomeController.cs
+++ TheBugTracker/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TheBugTracker.Extensions;
 using TheBugTracker.Models;
 using TheBugTracker.Models.ChartModels;
@@ -7,161 +8,161 @@ using TheBugTracker.Models.Enums;
 using TheBugTracker.Models.ViewModels;
 using TheBugTracker.Services.Interfaces;
 
-namespace TheBugTracker.Controllers
+namespace TheBugTracker.Controllers;
+
+public sealed class HomeController : Controller
 {
-    public sealed class HomeController : Controller
+    private readonly ICompanyInfoService _companyInfoService;
+    private readonly ILogger<HomeController> _logger;
+    private readonly IProjectService _projectService;
+
+    public HomeController(ILogger<HomeController> logger, ICompanyInfoService companyInfoService,
+        IProjectService projectService)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ICompanyInfoService _companyInfoService;
-        private readonly IProjectService _projectService;
+        _logger = logger;
+        _companyInfoService = companyInfoService;
+        _projectService = projectService;
+    }
 
-        public HomeController(ILogger<HomeController> logger, ICompanyInfoService companyInfoService, IProjectService projectService)
-        {
-            _logger = logger;
-            _companyInfoService = companyInfoService;
-            _projectService = projectService;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public async Task<IActionResult> Dashboard()
-        {
-            DashboardViewModel model = new();
-            int companyId = User.Identity.GetCompanyId().Value;
-
-            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
-            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId))
-                .Where(p=>p.Archived == false)
-                .ToList();
-
-            model.Tickets = model.Projects.SelectMany(p => p.Tickets)
-                .Where(t=>t.Archived == false)
-                .ToList();
     [Authorize]
     public async Task<IActionResult> Dashboard()
     {
         DashboardViewModel model = new();
         var companyId = User.Identity.GetCompanyId().Value;
 
-            model.Members = model.Company.Members.ToList();
+        model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+        model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId))
+            .Where(p => p.Archived == false)
+            .ToList();
 
-            return View(model);
-        }
+        model.Tickets = model.Projects.SelectMany(p => p.Tickets)
+            .Where(t => t.Archived == false)
+            .ToList();
 
-        [HttpPost]
-        public async Task<JsonResult> GglProjectPriority()
+        model.Members = model.Company.Members.ToList();
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> GglProjectPriority()
+    {
+        var companyId = User.Identity.GetCompanyId().Value;
+
+        var projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+        List<object> chartData = new();
+        chartData.Add(new object[] { "Priority", "Count" });
+
+
+        foreach (var priority in Enum.GetNames(typeof(ProjectPriorities)))
         {
-            int companyId = User.Identity.GetCompanyId().Value;
-
-            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
-
-            List<object> chartData = new();
-            chartData.Add(new object[] { "Priority", "Count" });
-
-
-            foreach (string priority in Enum.GetNames(typeof(ProjectPriorities)))
-            {
-                int priorityCount = (await _projectService.GetAllProjectsByPriorityAsync(companyId, priority)).Count();
-                chartData.Add(new object[] { priority, priorityCount });
-            }
-
-            return Json(chartData);
+            var priorityCount = (await _projectService.GetAllProjectsByPriorityAsync(companyId, priority)).Count();
+            chartData.Add(new object[] { priority, priorityCount });
         }
 
-        [HttpPost]
-        public async Task<JsonResult> GglProjectTickets()
+        return Json(chartData);
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> GglProjectTickets()
+    {
+        var companyId = User.Identity.GetCompanyId().Value;
+
+        var projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+        List<object> chartData = new();
+        chartData.Add(new object[] { "ProjectName", "TicketCount" });
+
+        foreach (var prj in projects)
         {
-            int companyId = User.Identity.GetCompanyId().Value;
-
-            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
-
-            List<object> chartData = new();
-            chartData.Add(new object[] { "ProjectName", "TicketCount" });
-
-            foreach (Project prj in projects)
-            {
-                chartData.Add(new object[] { prj.Name, prj.Tickets.Count() });
-            }
-
-            return Json(chartData);
+            chartData.Add(new object[] { prj.Name, prj.Tickets.Count() });
         }
 
-        public IActionResult Privacy()
+        return Json(chartData);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> AmCharts()
+    {
+        AmChartData amChartData = new();
+        List<AmItem> amItems = new();
+
+        var companyId = User.Identity.GetCompanyId().Value;
+
+        var projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false)
+            .ToList();
+
+        foreach (var project in projects)
         {
-            return View();
+            AmItem item = new();
+
+            item.Project = project.Name;
+            item.Tickets = project.Tickets.Count;
+            item.Developers = (await _projectService.GetProjectMembersByRoleAsync(project.Id, nameof(Roles.Developer)))
+                .Count();
+
+            amItems.Add(item);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        amChartData.Data = amItems.ToArray();
+
+
+        return Json(amChartData.Data);
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> PlotlyBarChart()
+    {
+        PlotlyBarData plotlyData = new();
+        List<PlotlyBar> barData = new();
+
+        var companyId = User.Identity.GetCompanyId().Value;
+
+        var projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+        //Bar One
+        PlotlyBar barOne = new()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            X = projects.Select(p => p.Name).ToArray(),
+            Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
+            Name = "Tickets",
+            Type = "bar"
+        };
 
-        [HttpPost]
-        public async Task<JsonResult> AmCharts()
+        //Bar Two
+        PlotlyBar barTwo = new()
         {
+            X = projects.Select(p => p.Name).ToArray(),
+            Y = projects
+                .Select(async p =>
+                    (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(Roles.Developer))).Count)
+                .Select(c => c.Result).ToArray(),
+            Name = "Developers",
+            Type = "bar"
+        };
 
-            AmChartData amChartData = new();
-            List<AmItem> amItems = new();
+        barData.Add(barOne);
+        barData.Add(barTwo);
 
-            int companyId = User.Identity.GetCompanyId().Value;
+        plotlyData.Data = barData;
 
-            List<Project> projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
-
-            foreach (Project project in projects)
-            {
-                AmItem item = new();
-
-                item.Project = project.Name;
-                item.Tickets = project.Tickets.Count;
-                item.Developers = (await _projectService.GetProjectMembersByRoleAsync(project.Id, nameof(Roles.Developer))).Count();
-
-                amItems.Add(item);
-            }
-
-            amChartData.Data = amItems.ToArray();
-
-
-            return Json(amChartData.Data);
-        }
-
-        [HttpPost]
-        public async Task<JsonResult> PlotlyBarChart()
-        {
-            PlotlyBarData plotlyData = new();
-            List<PlotlyBar> barData = new();
-
-            int companyId = User.Identity.GetCompanyId().Value;
-
-            List<Project> projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
-
-            //Bar One
-            PlotlyBar barOne = new()
-            {
-                X = projects.Select(p => p.Name).ToArray(),
-                Y = projects.SelectMany(p => p.Tickets).GroupBy(t => t.ProjectId).Select(g => g.Count()).ToArray(),
-                Name = "Tickets",
-                Type = "bar"
-            };
-
-            //Bar Two
-            PlotlyBar barTwo = new()
-            {
-                X = projects.Select(p => p.Name).ToArray(),
-                Y = projects.Select(async p => (await _projectService.GetProjectMembersByRoleAsync(p.Id, nameof(Roles.Developer))).Count).Select(c => c.Result).ToArray(),
-                Name = "Developers",
-                Type = "bar"
-            };
-
-            barData.Add(barOne);
-            barData.Add(barTwo);
-
-            plotlyData.Data = barData;
-
-            return Json(plotlyData);
-        }
+        return Json(plotlyData);
     }
 }
